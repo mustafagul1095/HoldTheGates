cc504d08d3fb2d6e3816e5e46b193a9c4001aa89
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-09-09 12:57:31 +0300

    TargetClosestEnemy Lecture

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -6,20 +6,52 @@ public class TargetLocator : MonoBehaviour
 {
 
     [SerializeField] private Transform weapon;
+    [SerializeField] private ParticleSystem projectileParticles;
+    [SerializeField] private int range = 20;
+    
     private Transform target;
-    private void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
 
     // Update is called once per frame
     private void Update()
     {
+        FindClosestTarget();
         AimWeapon();
     }
 
+    private void FindClosestTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float minDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (targetDistance < minDistance)
+            {
+                closestTarget = enemy.transform;
+                minDistance = targetDistance;
+            }
+        }
+        target = closestTarget;
+    }
+
+    private void Attack(bool isActive)
+    {
+        var emissionModule = projectileParticles.emission;
+        emissionModule.enabled = isActive;
+    }
     private void AimWeapon()
     {
+        float targetDistance = Vector3.Distance(target.position, transform.position);
         weapon.LookAt(target);
+        if (targetDistance <= range)
+        {
+            Attack(true);
+        }
+        else
+        {
+            Attack(false);
+        }
     }
 }
