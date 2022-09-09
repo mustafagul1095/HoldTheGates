bfacd38459c462a367f99f1a83bca6158c41f46a
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-09-09 15:36:35 +0300

    UI Text Lecture

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -3,25 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 public class Bank : MonoBehaviour
 {
     [SerializeField] private int startingBalance = 250;
+    
     [SerializeField] private int currentBalance = 250;
     public int CurrentBalance { get { return currentBalance; } }
 
+    [SerializeField] private TextMeshProUGUI displayBalance;
+    
     private void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplayBalance();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplayBalance();
     }
     
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplayBalance();
+        
         if (currentBalance < 0)
         {
             ReloadScene();
@@ -32,6 +41,11 @@ public class Bank : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    private void UpdateDisplayBalance()
+    {
+        displayBalance.text = $"Gold: {currentBalance}";
     }
     
 }
