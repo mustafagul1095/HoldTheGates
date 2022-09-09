d5b0e4eb1cc57a4d16e92f1c8bdb8f04931ce6c0
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-09-09 15:56:11 +0300

    Increasing Difficulty Lecture

--- Assets/Enemy/EnemyHealth.cs
+++ Assets/Enemy/EnemyHealth.cs
@@ -3,9 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int maxHitPoint = 10;
+    
+    [Tooltip("Adds max health to new enemies when one dies")]
+    [SerializeField] private int difficultyRamp = 1;
+    
     private int currentHp = 10;
     private Enemy enemy;
     // Start is called before the first frame update
@@ -31,6 +36,7 @@ public class EnemyHealth : MonoBehaviour
         {
             gameObject.SetActive(false);
             enemy.RewardGold();
+            maxHitPoint += difficultyRamp;
         }
     }
 }
